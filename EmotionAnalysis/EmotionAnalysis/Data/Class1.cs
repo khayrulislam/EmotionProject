a8688f0435798fe1defbbb444bf927224947584f
Khayrul Islam <khayrul822@outlook.com>
2019-03-04 23:39:22 +0600

    folder added

---
+++ EmotionAnalysis/EmotionAnalysis/Data/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmotionAnalysis.NewFolder
+{
+    class Class1
+    {
+    }
+}
--- EmotionAnalysis/EmotionAnalysis/Program.cs
+++ EmotionAnalysis/EmotionAnalysis/Program.cs
@@ -7,6 +7,7 @@ namespace EmotionAnalysis
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!   ");
+
         }
     }
 }
