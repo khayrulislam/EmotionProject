8eb6d744fb6b2c19a98192b7717d74996091b2b1
khayrul.islam <khayrul.islam@RAHI-PC>
2019-03-19 15:28:54 +0600

    add emotion class enitializer

--- EmotionAnalysis2/EmotionAnalysis/ResourceFolder/EmotionLookup.cs
+++ EmotionAnalysis2/EmotionAnalysis/ResourceFolder/EmotionLookup.cs
@@ -1,7 +1,36 @@
-﻿namespace EmotionAnalysis.ResourceFolder
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EmotionAnalysis.ResourceFolder
 {
     public class EmotionLookup
     {
-         
+        private readonly Dictionary<string, int> EmotionDictionary = new Dictionary<string, int>();
+        public bool initialize(string EmotionFileName)
+        {
+            if (File.Exists(EmotionFileName))
+            {
+                string[] lines = File.ReadAllLines(EmotionFileName);
+                foreach (string line in lines)
+                {
+                    string[] lineWords = line.Split('\t');
+                    EmotionDictionary[lineWords[0]] = Int32.Parse(lineWords[1]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} file doesn't exist", EmotionFileName);
+                return false;
+            }
+
+            return true;
+        }
+
+        public int getSentiStrengthValue(string word)
+        {
+            if (EmotionDictionary.ContainsKey(word)) return EmotionDictionary[word];
+            return 0;
+        }
     }
 }
--- EmotionAnalysis2/EmotionAnalysis/ResourceFolder/ResourceInitializer.cs
+++ EmotionAnalysis2/EmotionAnalysis/ResourceFolder/ResourceInitializer.cs
@@ -10,27 +10,28 @@ namespace EmotionAnalysis.ResourceFolder
     public class ResourceInitializer
     {
 
-        private BoosterWord boosterWord = new BoosterWord();
-        private EmoticonLookup emoticonLookup = new EmoticonLookup();
+        public BoosterWord boosterWord = new BoosterWord();
+        public EmoticonLookup emoticonLookup = new EmoticonLookup();
+        public EmotionLookup emotionLookup = new EmotionLookup();
 
         private string SentiStrengthDataFolder = @"..\..\SentiStrengthDataFolder";
-        private string BoosterWordListFile = "BoosterWordList.txt";
-        private string EmoticonLookupListFile = "EmoticonLookupTable.txt";
-        private string EmotionLookupListFile = "EmotionLookupTable.txt";
-        private string EnglishWordListFile = "EnglishWordList.txt";
-        private string IdiomLookupListFile = "IdiomLookupTable.txt";
-        private string NegatingWordListFile = "NegatingWordList.txt";
-        private string QuestionWordListFile = "QuestionWords.txt";
-        private string SlangLookupListFile = "SlangLookupTable.txt";
+        private string BoosterWordFile = "BoosterWordList.txt";
+        private string EmoticonLookupFile = "EmoticonLookupTable.txt";
+        private string EmotionLookupFile = "EmotionLookupTable.txt";
+        private string EnglishWordFile = "EnglishWordList.txt";
+        private string IdiomLookupFile = "IdiomLookupTable.txt";
+        private string NegatingWordFile = "NegatingWordList.txt";
+        private string QuestionWordFile = "QuestionWords.txt";
+        private string SlangLookupFile = "SlangLookupTable.txt";
 
         public void InitializeAllTheResources()
         {
 
-            if (boosterWord.initialize(Path.Combine(SentiStrengthDataFolder,BoosterWordListFile)) &&
-                emoticonLookup.initialize(Path.Combine(SentiStrengthDataFolder,EmoticonLookupListFile)) 
-                )
+            if (boosterWord.initialize(Path.Combine(SentiStrengthDataFolder,BoosterWordFile)) &&
+                emoticonLookup.initialize(Path.Combine(SentiStrengthDataFolder,EmoticonLookupFile)) &&
+                emotionLookup.initialize(Path.Combine(SentiStrengthDataFolder,EmotionLookupFile)))
             {
-                Console.WriteLine(emoticonLookup.getSentiStrengthValue("38*"));
+                Console.WriteLine(emotionLookup.getSentiStrengthValue("bug"));
                 Console.ReadLine();
             }
         }
