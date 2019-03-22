d8d16aac165b6df90a72b95e28d0bf5cb69016eb
Khayrul Islam <khayrul822@outlook.com>
2019-03-22 22:00:11 +0600

    repository data added and octokit library added

---
+++ EmotionAnalysis2/EmotionAnalysis/GitHubProjectAnalyzer/Data/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionAnalysis.GitHubProjectAnalyzer.Data
+{
+    public class Repository
+    {
+
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Description { get; set; }
+
+        public Uri RepositoryUrl { get; set; }
+
+        public Uri CollaboratorUrl { get; set; }
+
+        public Uri ContributorUrl { get; set; }
+
+        public Uri CommitUrl { get; set; }
+
+        public int CommitCount { get; set; }
+
+        public List<Commit> Commits { get; set; }
+
+        public Uri CommentUrl { get; set; }
+
+        public string Language { get; set; }
+
+        public Uri IssueUrl { get; set; }
+
+    }
+}
---
+++ EmotionAnalysis2/EmotionAnalysis/GitHubProjectAnalyzer/Data/RepositoryOwner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionAnalysis.GitHubProjectAnalyzer.Data
+{
+    public class RepositoryOwner
+    {
+        public string UserName { get; set; }
+
+        public Uri UserUrl { get; set; }
+
+        public Uri RepositoryUrl { get; set; }
+
+        public Uri GistUrl { get; set; }
+
+        public string UserType { get; set; }
+
+        public int RepositoryCount { get; set; }
+
+        public List<Repository> Repositories { get; set; }
+
+    }
+}
